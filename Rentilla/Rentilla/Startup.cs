dc3ec3d0c5b19fbb25242542b2a21cfeda9b6fc0
iPod90 <register@thomas-schalldach.de>
2015-11-25 19:12:17 +0200

    init

---
+++ Rentilla/Rentilla/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Rentilla.Startup))]
+namespace Rentilla
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
