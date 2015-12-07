e26c0ce07be4fcba6cbda4c64404b36cbad0dd74
Anthony Ferreol <anthonyf@metropolia.fi>
2015-12-07 13:35:32 +0100

    Hello

---
+++ Rentilla/Rentilla/Models/Categorie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Rentilla.Models
+{
+    public class Categorie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    public class CategorieDbContext : DbContext
+    {
+        public DbSet<Categorie> Categories { get; set; }
+    }
+}
---
+++ Rentilla/Rentilla/Models/Object.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Rentilla.Models
+{
+    public class Object
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Picture { get; set; }
+        public Categorie categorie { get; set; }
+    }
+    public class ObjectDbContext : DbContext
+    {
+        public DbSet<Object> Objects { get; set; }
+    }
+
+}
