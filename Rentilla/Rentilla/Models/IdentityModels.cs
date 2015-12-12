509bfbb9695518730f33148856d9a3075090631e
iPod90 <register@thomas-schalldach.de>
2015-12-12 17:23:05 +0200

    anotations

--- Rentilla/Rentilla/Models/IdentityModels.cs
+++ Rentilla/Rentilla/Models/IdentityModels.cs
@@ -4,21 +4,23 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Rentilla.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
-
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
-
+        [Display(Name = "Date of Birth")]
         public DateTime DateOfBirth { get; set; }
-
+        [Display(Name = "Profile Picture")]
         public String Picture { get; set; }
-
-        public Address Adress { get; set; }
+        [Display(Name = "Address")]
+        public Address Address { get; set; }
 
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
