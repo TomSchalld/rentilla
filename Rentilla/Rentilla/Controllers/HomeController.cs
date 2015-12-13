5998b93036021407dd1b0ce235ae8b0e0cbb61d8
iPod90 <register@thomas-schalldach.de>
2015-12-13 19:58:00 +0200

    Homescreen updt

--- Rentilla/Rentilla/Controllers/HomeController.cs
+++ Rentilla/Rentilla/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Rentilla.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +9,27 @@ namespace Rentilla.Controllers
 {
     public class HomeController : Controller
     {
+        private InterchangeDBContext db = new InterchangeDBContext();
+        private ApplicationDbContext adbc = new ApplicationDbContext();
         public ActionResult Index()
         {
+
+            var lastThreeOffers = (from o in db.Offers
+                                    orderby o.ID descending
+                                    select o).Take(3);
+            var lastThreeDemands = (from d in db.Demands
+                                   orderby d.ID descending
+                                   select d).Take(3);
+            foreach (var item in lastThreeOffers)
+            {
+                item.UID = (from u in adbc.Users where u.Id.Contains(item.UID) select u.UserName).FirstOrDefault();
+            }
+            foreach (var item in lastThreeDemands)
+            {
+                item.UID = (from u in adbc.Users where u.Id.Contains(item.UID) select u.UserName).FirstOrDefault();
+            }
+            ViewBag.LastThreeOffersList = lastThreeOffers.ToList();
+            ViewBag.LastThreeDemandsList = lastThreeDemands.ToList();
             return View();
         }
 
