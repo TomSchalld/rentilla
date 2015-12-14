f7b1a74ae35ea5e039975dc205cad16c65f4f567
anthonyferreol <anthony.ferreol@gmail.com>
2015-12-14 10:01:29 +0200

    ghjklm

---
+++ Rentilla/Rentilla/CustomFilters/AuthorizeAdminOrOwnerOfPostAttribute.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+
+namespace A11_RBS.CustomFilters
+{
+    public class AuthorizeAdminOrOwnerOfPostAttribute : AuthorizeAttribute
+    {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            var authorized = base.AuthorizeCore(httpContext);
+            if (!authorized)
+            {
+                // The user is not authenticated
+                return false;
+            }
+
+            var user = httpContext.User;
+            if (user.IsInRole("administrator"))
+            {
+                // Administrator => let him in
+                return true;
+            }
+
+            var rd = httpContext.Request.RequestContext.RouteData;
+            var id = rd.Values["id"] as string;
+            if (string.IsNullOrEmpty(id))
+            {
+                // No id was specified => we do not allow access
+                return false;
+            }
+
+            return IsOwnerOfPost(user.Identity.Name, id);
+        }
+
+        private bool IsOwnerOfPost(string username, string postId)
+        {
+            // TODO: you know what to do here
+            throw new NotImplementedException();
+        }
+    }
+}
